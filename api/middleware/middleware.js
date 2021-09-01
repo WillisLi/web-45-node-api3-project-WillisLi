e61b3549ddfb87ce0e9228974e11c082b686b78b
WillisLi <li.willis95@gmail.com>
2021-09-01 13:50:01 -0700

    completed mvp

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,45 @@
+const Users = require('./../users/users-model');
+const Posts = require('./../posts/posts-model');
+const { response } = require('../server');
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  const url = req.url;
+  const method = req.method;
+  const timestamp = Date.now();
+  console.log(`${method}, ${url}, ${timestamp}`)
+  next();
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  const userId = req.params.id;
+  Users.findById(userId)
+    .then(user => {
+      if(user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(404).json({ message: "user not found" });
+      }
+    })
+    .catch(next)
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  const user = req.body;
+  if (!user.name) {
+    next({status: 400, message: "missing required name field" });
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  const post = req.body;
+  if (!post.text) {
+    next({status: 400, message: "missing required text field" });
+  } else {
+    next();
+  }
 }
 
-// do not forget to expose these functions to other modules
+module.exports = { logger, validateUserId, validateUser, validatePost }
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,7 @@
 const express = require('express');
-
+const Users = require('./users-model');
+const Posts = require('./../posts/posts-model');
+const { validatePost, validateUserId, validateUser} = require('./../middleware/middleware');
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 
@@ -7,38 +9,73 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  Users.get()
+    .then(usersList => {
+      res.status(200).json(usersList);
+    })
+    .catch(error => {
+      res.status(404).json({message: error})
+    })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  res.json(req.user)
 });
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  Users.insert(req.body)
+    .then(newUser => {
+      res.status(201).json(newUser);
+    })
+    .catch(next);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  Users.update(req.params.id, req.body)
+    .then(updatedUser => {
+      res.status(200).json(updatedUser);
+    })
+    .catch(next);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res, next) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  Users.remove(req.params.id)
+    .then(deletedUser => {
+      res.json(deletedUser);
+    })
+    .catch(next);
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res, next) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  Users.getUserPosts(req.params.id)
+    .then(postsList => {
+      res.status(200).json(postsList);
+    })
+    .catch(next)
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res, next) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  Posts.insert(req.body)
+    .then(newPost => {
+      res.status(201).json(newPost);
+    })
+    .catch(next)
 });
 
 // do not forget to export the router
+
+module.exports = router;
